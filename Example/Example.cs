66d28ed8420d05ba563e09bf0398cff9d9f9368b
Paul Hayes <paul.hayes@entropedia.co.uk>
2014-10-12 16:26:47 +0100

    Removed first attempt

--- Example/Example.cs
+++ Example/Example.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 public class Example : MonoBehaviour {
 
 	public ResourceGameObject resourceAsset;
-	public ResourceAsset<ExampleScriptableObject> scritableObjectExample;
 	public ResourceAudioClip audioClipResource;
 	public ResourceTexture textureResource;
 	public List<ResourceGameObject> resourceList;
